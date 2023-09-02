db5d4022963a23dac3d362719ae8a9ff0988b846
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2023-09-02 21:37:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
