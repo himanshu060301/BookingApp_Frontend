e638b0232f2e0b185f06e6188f3fa0ce721f2a42
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2023-10-08 18:11:25 +0530

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,7 +4,7 @@ import "./featured.css";
 const Featured = () => {
 
   const {data,loading,error}=useFetch("/hotels/countByCity?cities=berlin,madrid,london");
-  
+  console.log(data);
   return (
     <div className="featured">
       {loading ? ("Loading please wait"
